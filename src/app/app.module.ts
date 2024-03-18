77bc7a7c0cff498e7d77c3acd3ca86a96ca6e790
unknown <kuba@fiku.eu>
2024-03-18 17:47:29 +0100

    setup

---
+++ src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import {HttpClientModule} from "@angular/common/http";
+import {BrowserModule} from "@angular/platform-browser";
+
+@NgModule({
+  declarations: [],
+  imports: [
+    HttpClientModule,
+    BrowserModule
+  ]
+})
+export class AppModule { }
